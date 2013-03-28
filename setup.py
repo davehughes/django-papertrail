47b820cea668dbc7653c97e0189f9a1ff1f2e64a
David Hughes <d@vidhughes.com>
2013-03-28 10:07:43 -0700

    Fixed dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setuptools.setup(
     name='papertrail',
     version='0.1.0',
     packages=setuptools.find_packages(),
+    dependency_links=[
+        'https://github.com/shlomozippel/django-apptemplates#egg=django-apptemplates',
+        ],
     install_requires=[
         'django-jsonfield>=0.8.11',
-        'django-apptemplates==0.0.1',
+        'django-apptemplates',
         ]
 )
